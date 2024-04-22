e8595168d36679bee8e8618ebbbafba3cfb540f3
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2024-04-22 14:59:49 -0300

    Initial Commit

---
+++ big-clock/big_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  big_clockApp.swift
+//  big-clock
+//
+//  Created by Bruno Duarte on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct big_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
