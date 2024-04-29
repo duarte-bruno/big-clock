1e505421451be2ef68c3006f874e7e4f754a46a2
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2024-04-29 10:14:28 -0300

    Updated the year view layout

--- big-clock/Components/YearView.swift
+++ big-clock/Components/YearView.swift
@@ -9,15 +9,25 @@ struct YearView: View {
     // MARK: - View
     
     var body: some View {
-        VStack(alignment: .leading, content: {
-            Text(viewModel.mainYearText)
-                .font(.system(size: 14, weight: .regular))
-                .multilineTextAlignment(.leading)
+        HStack(alignment: .center, content: {
+            Image(systemName: "calendar")
+                .font(.title)
                 .foregroundStyle(.white)
-                .padding(.horizontal, 0)
-            ProgressView(value: viewModel.yearPastPercentage, total: 100)
-                .tint(.white)
+                .padding(0)
+                .padding(.trailing, 5)
+            
+            VStack(alignment: .leading, content: {  
+                Text(viewModel.mainYearText)
+                    .font(.system(size: 14, weight: .regular))
+                    .multilineTextAlignment(.leading)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 0)
+                ProgressView(value: viewModel.yearPastPercentage, total: 100)
+                    .tint(.white)
+            })
+            .frame(maxWidth: .infinity)
         })
+       
     }
 }
 
